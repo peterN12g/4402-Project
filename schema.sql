ac12c2bf49392171475f614db4483c78d0c34ac1
nichady <nichthai2004@gmail.com>
2024-11-24 16:07:26 -0600

    add schema

---
+++ schema.sql
@@ -0,0 +1,59 @@
+-- entity
+CREATE TABLE users (
+    username TEXT,
+    password TEXT,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+
+    PRIMARY KEY (username);
+);
+
+-- relation
+CREATE TABLE friends (
+    username1 TEXT,
+    username2 TEXT,
+    accepted BOOLEAN,
+
+    FOREIGN KEY (username1) REFERENCES users(username);
+    FOREIGN KEY (username2) REFERENCES users(username);
+);
+
+-- entity
+CREATE TABLE posts (
+    id INTEGER AUTOINCREMENT,
+    username TEXT,
+    description TEXT,
+    image BLOB,
+
+    PRIMARY KEY (id);
+    FOREIGN KEY (username) REFERENCES users(username);
+);
+
+-- entity
+CREATE TABLE comments (
+    id INTEGER AUTOINCREMENT,
+    post_id INTEGER,
+    username TEXT,
+    text TEXT,
+
+    PRIMARY KEY (id);
+    FOREIGN KEY (post_id) REFERENCES posts(id)
+    FOREIGN KEY (username) REFERENCES users(username)
+);
+
+-- relation
+CREATE TABLE post_likes (
+    post_id INTEGER,
+    username TEXT,
+
+    FOREIGN KEY (post_id) REFERENCES posts(id)
+    FOREIGN KEY (username) REFERENCES users(username)
+)
+
+-- relation
+CREATE TABLE comment_likes(
+    comment_id INTEGER,
+    username TEXT,
+
+    FOREIGN KEY (comment_id) REFERENCES comments(id)
+    FOREIGN KEY (username) REFERENCES users(username)
+)
