1f8363dc3071cab970ce27eb1b607127627118b1
nichady <nichthai2004@gmail.com>
2024-11-25 21:11:06 -0600

    enhance database regeneration

--- sql/regenerate.js
+++ sql/regenerate.js
@@ -2,20 +2,45 @@
 
 import { open } from "sqlite";
 import sqlite3 from "sqlite3";
-import { readFile, rm } from "fs/promises";
+import { readFile, rm, access, constants } from "node:fs/promises";
+
+const dbFile = "database.sqlite3";
 
 const schema = await readFile("sql/schema.sql", "utf-8");
 const populate = await readFile("sql/populate.sql", "utf-8");
 
+async function removeExistingDatabase() {
+    try {
+        await access(dbFile, constants.F_OK);
+    } catch(e) {
+        return;
+    }
+    
+    console.log("removing existing database...");
+    await rm(dbFile);
+}
 
-console.log("Reseting database...")
+async function createDatabase() {
+    console.log("creating database...");
+    return await open({
+        filename: dbFile,
+        mode: sqlite3.OPEN_CREATE | sqlite3.OPEN_READWRITE,
+        driver: sqlite3.Database,
+    });
+}
 
+async function addTables(db) {
+    console.log("adding tables...");
+    await db.exec(schema);
+}
 
-await rm("./sqlite.db", { force: true });
-const db = await open({
-    filename: "./sqlite.db",
-    driver: sqlite3.Database,
-});
+async function populateTables(db) {
+    console.log("populating tables...");
+    await db.exec(populate);
+}
 
-db.exec(schema);
-db.exec(populate);
+await removeExistingDatabase();
+const db = await createDatabase();
+await addTables(db);
+await populateTables(db);
+console.log("done")
