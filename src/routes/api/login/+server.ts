e0bf5c8e122ea341ebbd298b4fd450facfa3d8dd
nichady <nichthai2004@gmail.com>
2024-12-02 17:58:03 -0600

    use sql-template-tag

--- src/routes/api/login/+server.ts
+++ src/routes/api/login/+server.ts
@@ -1,10 +1,11 @@
 import { error } from '@sveltejs/kit';
 import { createToken, db } from '$lib/server';
+import sql from "sql-template-tag";
 
 export async function POST({ request, cookies }) {
 	const { username, password } = await request.json();
 	if (typeof username !== "string" || typeof password != "string") error(400);
-	const result = await db.get(`SELECT 1 FROM users WHERE username = ? AND password = ?`, username, password);
+	const result = await db.get(sql`SELECT 1 FROM users WHERE username = ${username} AND password = ${password}`);
 
 	if (!result) error(401);
 
